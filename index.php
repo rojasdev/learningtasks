f5411e3fd9246ffb66dc0d304d07edf4b6d6ce6f
rojasdev <57605764+rojasdev@users.noreply.github.com>
2022-02-16 12:18:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
       <h1>NAME MO WEB YOUR FRIENDLY WEB DESIGNER</h1>
       <h2>Previous Works</h2>
       <ul>
-        <li><a href="https://namemoportfolio.herokuapp.com/thesimpsons">The Simpsons Website</a></li>
-        <li><a href="/LT2">Learning Task 2.1</a></li>
+        <li><a href="https://namemoportfolio.herokuapp.com/thesimpsons" target="black">The Simpsons Website</a></li>
+        <li><a href="/LT2" target="black">Learning Task 2.1</a></li>
       </ul>
     </body>
 </html>
