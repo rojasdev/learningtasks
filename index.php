e3432d1da504279aa126a8369d0931329694e1e3
rojasdev <57605764+rojasdev@users.noreply.github.com>
2022-02-16 12:15:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <!DOCTYPE html>
+<html>
+    <head>
+        <title>NAME MO WEB PORTFOLIO</title>
+    </head>
+    <body>
+      <h1>NAME MO WEB YOUR FRIENDLY WEB DESIGNER</h1>
+      <h2>Previous Works</h2>
+      <ul>
+        <li><a href="https://namemoportfolio.herokuapp.com/the simpsons">The Simpsons Website</a></li>
+        <li><a href="/LT2">Learning Task 2.1</a></li>
+      </ul>
+    </body>
+</html>>
